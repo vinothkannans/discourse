8d9d81e6c940df662e0f4fb9a3501db2ea6dcc62
Vinoth Kannan <vinothkannan@vinkas.com>
2018-11-24 16:28:18 +0530

    FIX: category images have no sizes

--- app/serializers/category_upload_serializer.rb
+++ app/serializers/category_upload_serializer.rb
@@ -1,7 +1,3 @@
 class CategoryUploadSerializer < ApplicationSerializer
-  attributes :id, :url, :aspect_ratio
-  
-  def aspect_ratio
-    "--aspect-ratio:#{object.width}/#{object.height}"
-  end
+  attributes :id, :url, :width, :height
 end
